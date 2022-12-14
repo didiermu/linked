32cd272812823fe8f708b74e8b4157df1c8b142e
didier-mu <didier.m@bnn.mx>
2022-12-14 00:46:17 -0600

    fix: mobile qa1

--- src/js/forms.js
+++ src/js/forms.js
@@ -1,28 +1,9 @@
-const focusInput = () => {
-    const inputs = document.querySelectorAll(".input__group input");
-
-    for (const inputsElem of inputs) {
-        inputsElem.addEventListener("click", () => {
-            inputsElem.focus();
-            inputsElem.parentNode.classList.add("active");
-            inputsElem.parentNode.classList.remove("valid");
-        });
-
-        inputsElem.addEventListener("blur", () => {
-            if (inputsElem.value == "") {
-                inputsElem.parentNode.classList.remove("active");
-            } else {
-                inputsElem.parentNode.classList.replace("active", "valid");
-            }
-        });
-    }
-};
-
 /// EMAIL
 
 const valMail = (nameInput) => {
     if (nameInput.value.length < 1) {
-        nameInput.parentNode
+        nameInput
+            .closest(".input__group")
             .querySelector(".error-input")
             .classList.add("show");
     } else {
@@ -30,19 +11,22 @@ const valMail = (nameInput) => {
             /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         var validNumbre = regEx.test(nameInput.value);
         if (!validNumbre) {
-            nameInput.parentNode
+            nameInput
+                .closest(".input__group")
                 .querySelector(".error-input")
                 .classList.add("show");
-            nameInput.parentNode.querySelector(".error-input").innerText =
+            nameInput
+                .closest(".input__group")
+                .querySelector(".error-input").innerText =
                 "Verifica el formato de email ";
         } else {
-            nameInput.parentNode
+            nameInput
+                .closest(".input__group")
                 .querySelector(".error-input")
                 .classList.remove("show");
         }
     }
 };
-
 /// TEXTO
 
 const valText = (nameInput) => {
@@ -51,22 +35,28 @@ const valText = (nameInput) => {
     var validText = regEx.test(nameInput.value);
 
     if (nameInput.value.length < 1) {
-        nameInput.parentNode
+        nameInput
+            .closest(".input__group")
             .querySelector(".error-input")
             .classList.add("show");
     } else {
-        nameInput.parentNode
+        nameInput
+            .closest(".input__group")
             .querySelector(".error-input")
             .classList.remove("show");
 
         if (!validText) {
-            nameInput.parentNode
+            nameInput
+                .closest(".input__group")
                 .querySelector(".error-input")
                 .classList.add("show");
-            nameInput.parentNode.querySelector(".error-input").innerText =
+            nameInput
+                .closest(".input__group")
+                .querySelector(".error-input").innerText =
                 "Solo se permiten letras";
         } else {
-            nameInput.parentNode
+            nameInput
+                .closest(".input__group")
                 .querySelector(".error-input")
                 .classList.remove("show");
         }
@@ -77,7 +67,8 @@ const valText = (nameInput) => {
 
 const valNum = (nameInput) => {
     if (nameInput.value.length < 1) {
-        nameInput.parentNode
+        nameInput
+            .closest(".input__group")
             .querySelector(".error-input")
             .classList.add("show");
     } else {
@@ -85,13 +76,17 @@ const valNum = (nameInput) => {
         var validEmail = regEx.test(nameInput.value);
 
         if (!validEmail) {
-            nameInput.parentNode
+            nameInput
+                .closest(".input__group")
                 .querySelector(".error-input")
                 .classList.add("show");
-            nameInput.parentNode.querySelector(".error-input").innerText =
+            nameInput
+                .closest(".input__group")
+                .querySelector(".error-input").innerText =
                 "Solo se permiten números";
         } else {
-            nameInput.parentNode
+            nameInput
+                .closest(".input__group")
                 .querySelector(".error-input")
                 .classList.remove("show");
         }
@@ -102,7 +97,8 @@ const valNum = (nameInput) => {
 
 const valTextNum = (nameInput) => {
     if (nameInput.value.length < 1) {
-        nameInput.parentNode
+        nameInput
+            .closest(".input__group")
             .querySelector(".error-input")
             .classList.add("show");
     } else {
@@ -111,13 +107,17 @@ const valTextNum = (nameInput) => {
         var validText = regEx.test(nameInput.value);
 
         if (!validText) {
-            nameInput.parentNode
+            nameInput
+                .closest(".input__group")
                 .querySelector(".error-input")
                 .classList.add("show");
-            nameInput.parentNode.querySelector(".error-input").innerText =
+            nameInput
+                .closest(".input__group")
+                .querySelector(".error-input").innerText =
                 "No se permiten caracteres especiales";
         } else {
-            nameInput.parentNode
+            nameInput
+                .closest(".input__group")
                 .querySelector(".error-input")
                 .classList.remove("show");
         }
@@ -129,12 +129,12 @@ const valTextNum = (nameInput) => {
 const valFecha = (nameInput) => {
     if (nameInput.value.length < 1) {
         nameInput
-            .closest(".input")
+            .closest(".input__group")
             .querySelector(".error-input")
             .classList.add("show");
     } else {
         nameInput
-            .closest(".input")
+            .closest(".input__group")
             .querySelector(".error-input")
             .classList.remove("show");
     }
@@ -176,32 +176,31 @@ const valCombos = (nameInput) => {
 };
 
 const validarForm = () => {
+    console.log("so");
     const form = document.querySelector("#form-contacto"),
         btnContacto = document.querySelector("#btn-connect"),
         inputNombre = document.querySelector("#input-name"),
-        inputApellido = document.querySelector("#input-email"),
-        inputEmpresa = document.querySelector("#input-phone"),
-        inputCorreo = document.querySelector("#input-country"),
-        inputNumero = document.querySelector("#input-purpose"),
-        inputAsunto = document.querySelector("#input-why"),
+        inputEmail = document.querySelector("#input-email"),
+        inputPhone = document.querySelector("#input-phone"),
+        inputCountry = document.querySelector("#input-country"),
         inputMsj = document.querySelector("#msj-contacto");
 
     btnContacto.onclick = () => {
         valText(inputNombre);
-        valText(inputApellido);
-        valText(inputEmpresa);
-        valMail(inputCorreo);
-        valNum(inputNumero);
-        valText(inputAsunto);
-        valText(inputLugar);
-        valCombos(inputTipoEvento);
+        valMail(inputEmail);
+        valNum(inputPhone);
+        valText(inputCountry);
+        valTextNum(inputMsj);
 
         let msjsError = document.querySelectorAll(".error-input.show");
 
         if (msjsError.length == 0) {
             // form.submit();
             console.log("si");
-            return true;
+            btnContacto.classList.add("active");
+            btnContacto.innerText = "SENDED";
+            form.reset();
+            return false;
         } else {
             console.log("no");
             return false;
